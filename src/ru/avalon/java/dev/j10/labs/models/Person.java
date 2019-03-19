bc4bcc4f53a75fa3b62ad8c1f661bfa1293a30a3
moskovanton <48486058+moskovanton@users.noreply.github.com>
2019-03-19 14:52:37 +0300

    Update Person.java

--- src/ru/avalon/java/dev/j10/labs/models/Person.java
+++ src/ru/avalon/java/dev/j10/labs/models/Person.java
@@ -1,30 +1,28 @@
 package ru.avalon.java.dev.j10.labs.models;
 
-import ru.avalon.java.dev.j10.labs.commons.Address;
 /**
  * Представление о человеке.
- * 
+ * <p>
  * С точки зрения задания человек представляется как сущность,
  * наделённая:
- *     - именем;
- *     - паспортными данными;
- *     - пропиской по месту жительства.
- *
+ * <ol>
+ *     <li>именем;
+ *     <li>паспортными данными;
+ *     <li>пропиской по месту жительства.
+ * </ol>
  */
 public class Person {
-    
-    public Passport passport;
-    public Address address;
+
     /*
      * TODO(Студент): Создайте класс Address.
      *
-     * 1. Добавьте файл в пакет ru.avalon.java.dev.j10.labs.commons.
+     * 1. Добавте файл в пакет ru.avalon.java.dev.j10.labs.commons.
      *
      * 2. Создайте класс, видимый из пакета. Подумайте о том
-     *    Какое имя должен иметь класс, если он объявлен в этом
+     *    Какое имя должен иметь класс, если он объявленн в этом
      *    файле.
      *
-     * 3. Подумайте над тем, какие переменные должны быть
+     * 3. Подумайте над тем, какие переменные должены быть
      *    определены в классе.
      *
      * 4. Подумайте над тем, какие методы должны быть объявлены
@@ -32,16 +30,16 @@ public class Person {
      */
 
     /**
-     * Возвращает полное имя человека.
-     * 
+     * Возврвщает полное имя человека.
+     * <p>
      * Если у человека есть Имя, Фамилия и Отчество, то
-     * возвращает Имя, Фамилию и Отчество, разделённые пробелом.
-     * 
+     * возвращет Имя, Фимилию и Отчество, разделённые пробелом.
+     * <p>
      * Если у человека нет Отчества, но есть второе имя, то
      * возвращает Имя, Первую букву второго имени, и Фамилию,
      * разделённые пробелом. После Инициала второго имени
      * должна стоять точка. Например, "Джером К. Джером".
-     * 
+     * <p>
      * Если у человека нет ни Отчества ни Второго имени, а
      * есть только Имя и Фамилия, то возвращает их, разделённые
      * пробелом.
@@ -52,21 +50,12 @@ public class Person {
         /*
          * TODO(Студент): Закончить определение метода 'getFullName()' класса 'Person'
          */
-        
-        String s = passport.getName() + " ";
-        if (!passport.getPatronymic().equals("")){
-            s += passport.getPatronymic() + " ";
-        }
-        else {
-            s += passport.getSecondName().charAt(0) + ". "; 
-        }
-        s += passport.getSurname() + ".";
-        return s;
+        return null;
     }
 
     /**
      * Возвращает адрес, по которому проживает человек.
-     * 
+     * <p>
      * Возвращаемый адрес соответствует месту постоянной
      * регистрации человека, согласно паспортным данным.
      *
@@ -75,21 +64,7 @@ public class Person {
     public String getAddress() {
         /*
          * TODO(Студент): Закончить определение метода 'getAddress()' класса 'Person'
-         */ 
-        String s = address.getCountry() + ", " + address.getCity() + ", " + address.getStreet() + ", д." + address.getHouseNumber() + ", ";
-        if (!(address.getHullNumber() == 0)){
-            s += "к." + address.getHullNumber() + ", ";
-        }
-        if (!(address.getApartmentNumber() == 0)){
-            s += "кв." + address.getApartmentNumber() + ", ";
-        }
-        s += "индекс " + address.getIndex() + ".";
-        return s;
+         */
+        return null;
     }
-
-    public Person(Passport passport, Address address) {
-        this.passport = passport;
-        this.address = address;
-    }
-
 }
